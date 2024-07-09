4b0185ed12c026b7f5a615d9191392a6ab65d168
dduyanhhoang <dduyanhhoang@gmail.com>
2024-07-09 09:08:29 +0700

    feat: Add ArrayQueue implementation for queue data structure

---
+++ queue/array_queue.py
@@ -0,0 +1,62 @@
+from empty import Empty
+
+
+class ArrayQueue:
+    DEFAULT_CAPACITY = 10
+
+    def __init__(self):
+        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+
+    def __len__(self):
+        return self._size
+    
+    def is_empty(self):
+        return self._size == 0
+    
+    def first(self):
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        return self._data[self._front]
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty('Queue is empty')
+        answer = self._data[self._front]
+        self._data[self._front] = None
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
+    
+    def enqueue(self, e):
+        if self._size == len(self._data):
+            self._resize(2 * len(self._data))
+        avail = (self._front + self._size) % len(self._data)
+        self._data[avail] = e
+        self._size += 1
+
+    def _resize(self, cap):
+        old = self._data
+        self._data = [None] * cap
+        walk = self._front
+        for k in range(self._size):
+            self._data[k] = old[walk]
+            walk = (1 + walk) % len(old)
+        self._front = 0
+
+    def __str__(self):
+        return str(self._data)
+
+
+if __name__ == '__main__':
+    aq = ArrayQueue()
+    aq.enqueue(5)
+    aq.enqueue(3)
+    print(aq)
+    print(aq._front)
+    print(aq.dequeue())
+    print(aq)
+    print(aq._front)
+    aq.enqueue(7)
+    print(aq)
